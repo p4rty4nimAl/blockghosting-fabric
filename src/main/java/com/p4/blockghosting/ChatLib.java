cc209af2285ebcb15e23b12da851db60b7d13287
p4rty4nimAl <actuallyp4@gmail.com>
2022-04-17 20:15:31 +0100

    small optimisation, general admin

--- src/main/java/com/p4/blockghosting/ChatLib.java
+++ src/main/java/com/p4/blockghosting/ChatLib.java
@@ -1,10 +1,12 @@
 package com.p4.blockghosting;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.text.LiteralText;
 
 public class ChatLib {
+    private static final ChatHud chatHud = MinecraftClient.getInstance().inGameHud.getChatHud();
     public static void chat(LiteralText text) {
-        MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(text);
+        chatHud.addMessage(text);
     }
 }
